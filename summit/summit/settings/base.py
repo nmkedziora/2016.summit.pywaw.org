d91639e5c16735fc1485f9fbad0728fbfeffbef8
konradhalas <halas.konrad@gmail.com>
2015-11-25 19:27:44 +0100

    Fix static path.

--- summit/summit/settings/base.py
+++ summit/summit/settings/base.py
@@ -92,7 +92,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/2016/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
